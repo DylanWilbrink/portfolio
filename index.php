a82a86a1d8deb55fde13fc3c10703c18a840ab57
jaimyvangerrevink <36194767+jaimyvangerrevink@users.noreply.github.com>
2018-11-27 15:16:16 +0100

    addhome.php -> index.php
    was begonnen op addhome.php maar moet homepage zijn.. Alstu.

--- index.php
+++ index.php
@@ -1,74 +1,110 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <title><?php if (isset($_SESSION['logged_in'])) {
-            echo 'Welkom - ' . $_SESSION['email'];
-        } ?></title>
-    <?php
-    session_start();
-    include("includes/includes.php");
-    ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Material Design Bootstrap</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="stylesheets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="stylesheets/css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="stylesheets/css/style.css" rel="stylesheet">
+
+
+
+    <!--css-->
+    <link rel="stylesheet" href="js/swiper/css/swiper.css">
+    <!--js-->
+    <script src="js/swiper/js/swiper.js"></script>
+    <script src="js/swiper/js/swiper.esm.js"></script>
 </head>
+
 <body>
-<!-- Nav begin-->
-<?php include 'includes/navbar_index.php';
+<!-- Start your project here-->
+<?php
+include('includes/navbar_index.php');
 ?>
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card card-image" style="background-image: url(Header.jpg); background-size: cover;
-  background-position: 50% 90%;">
-                <div class="text-white text-center py-5 px-4 my-5">
-                    <div>
-                        <h1 class="card-title pt-3 mb-5 font-bold"><strong>Waar ga jij heen op vakatie?</strong></h1>
-                        <a class="btn btn-outline-white btn-rounded" style="border-radius: 90px;"><i class="fa fa-home left"></i> Bekijk onze vakantiehuizen</a>
+<div class="card jumbocard card-image" style="background-image: url(Header.jpg);">
+    <div class="container quote-container py-5 my-5">
+        <div class="quote">
+            <div class="text">
+                Quote van de persoon.
+            </div>
+            <div class="text-bottom">
+                Naam/functie of iets dergelijks
+            </div>
+            <div class="link-button">
+                <a class="link" href="#link">Meer over onderwerp</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container my-4">
+    <div class="swiper-container sliderprojecten">
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide">
+                <div class="av-card" style="height: 350px; margin: 2px; box-shadow: 0 0 0 2px #404040;">
+                    <img src="http://placehold.jp/7464ac/ffffff/1920x1080.png?text=Afbeelding van project" alt="" style="width: 100%; height: 50%; object-fit: cover;">
+                    <div class="av-text" style="padding: 24px">
+                        <h2 style="line-height: 24px; font-size: 22px; font-family: the_mix_bold; color: #404040">Titel van het project</h2>
+                        <p>omschrijving van project.</p>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
-
-<div class="container">
-    <div class="row">
-
-        <div class="col-md-12 mt-5">
-            <h4>Dit zijn onze laatste vakantiehuizen:</h4>
-        </div>
-
-        <div class="col-md-6 col-lg-4">
-            <div class="card mt-5">
-                <!--                  <div class="card-header primary-color white-text">-->
-                <!--                      Naam van vakantiehuis.-->
-                <!--                  </div>-->
-                <img class="card-img" src="https://images.unsplash.com/photo-1533667586627-9f5ddbd42539?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bb707c0b87345277ebc415156111fb69&auto=format&fit=crop&w=1920&h=1080&q=60" alt="Card image cap">
-                <div class="card-body">
-                    <h4 class="card-title"><a>Card title</a></h4>
-                    <hr />
-                    <p class="card-text">Huis beschrijving. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem, optio vero odio nam sit officia.</p>
-                    <a href="" style="border-radius: 90px;" class="btn btn-primary">Button</a>
+            <div class="swiper-slide">
+                <div class="av-card" style="height: 350px; margin: 2px; box-shadow: 0 0 0 2px #404040;">
+                    <img src="http://placehold.jp/3EB1C8/ffffff/1920x1080.png?text=Afbeelding van project" alt="" style="width: 100%; height: 50%; object-fit: cover;">
+                    <div class="av-text" style="padding: 24px">
+                        <h2 style="line-height: 24px; font-size: 22px; font-family: the_mix_bold; color: #404040">Titel van het project</h2>
+                        <p>omschrijving van project.</p>
+                    </div>
                 </div>
             </div>
-        </div>
-
-        <div class="col-md-6 mb-5 col-lg-4">
-            <div class="card mt-5">
-                <!--                  <div class="card-header primary-color white-text">-->
-                <!--                      Naam van vakantiehuis.-->
-                <!--                  </div>-->
-                <img class="card-img" src="https://images.unsplash.com/photo-1533667586627-9f5ddbd42539?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bb707c0b87345277ebc415156111fb69&auto=format&fit=crop&w=1920&h=1080&q=60" alt="Card image cap">
-                <div class="card-body">
-                    <h4 class="card-title"><a>Card title</a></h4>
-                    <hr />
-                    <p class="card-text">Huis beschrijving. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem, optio vero odio nam sit officia.</p>
-                    <a href="" style="border-radius: 90px;" class="btn btn-primary">Button</a>
+            <div class="swiper-slide">
+                <div class="av-card" style="height: 350px; margin: 2px; box-shadow: 0 0 0 2px #404040;">
+                    <img src="http://placehold.jp/ea7600/ffffff/1920x1080.png?text=Afbeelding van project" alt="" style="width: 100%; height: 50%; object-fit: cover;">
+                    <div class="av-text" style="padding: 24px">
+                        <h2 style="line-height: 24px; font-size: 22px; font-family: the_mix_bold; color: #404040">Titel van het project</h2>
+                        <p>omschrijving van project.</p>
+                    </div>
                 </div>
             </div>
-        </div>
 
+        </div>
+        <div class="swiper-pagination"></div>
     </div>
+    <script>
+        var swiper = new Swiper('.sliderprojecten', {
+            speed: 1000,
+            slidesPerView: 3,
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+            },
+            spaceBetween: 24,
+            loop: true,
+            // effect: 'fade',
+            coverflowEffect: {
+                rotate: 50,
+                slideShadows: false,
+            },
+            breakpoints: {
+                768: {
+                    slidesPerView: 2
+                }
+            },
+        });
+    </script>
 </div>
-<!-- Footer -->
+
+
 <!-- SCRIPTS -->
 <!-- JQuery -->
 <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
@@ -78,5 +114,7 @@
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <!-- MDB core JavaScript -->
 <script type="text/javascript" src="js/mdb.min.js"></script>
+
 </body>
+
 </html>
