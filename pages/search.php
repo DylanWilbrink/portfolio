8dbea068d7ec7642f8edb8634c0dacf2c7de476e
DylanWilbrink <dylanwilbrink@gmail.com>
2018-11-29 11:38:15 +0100

    Updated search

--- pages/search.php
+++ pages/search.php
@@ -1,8 +1,9 @@
 <html>
 <head>
     <?php include('../php/homes.php');
+    $submit = isset($_POST['submit']) ? $_POST['submit'] : '';
 
-    if($_POST['submit']) {
+    if ($submit) {
         $show = $connection->connect()->prepare("SELECT * FROM projects p
                                                   WHERE p.project_name LIKE :search OR p.project_creators LIKE :search
                                                   ORDER BY p.project_id DESC");
@@ -27,7 +28,6 @@
     <link href="../stylesheets/css/style.css" rel="stylesheet">
 
 
-
     <!--css-->
     <link rel="stylesheet" href="js/swiper/css/swiper.css">
     <!--js-->
@@ -40,18 +40,37 @@ include('../includes/navbar.php');
 ?>
 
 <div class="container mt-4">
-        <h2 class="intro-kop">Titeltext voor de searchpage</h2>
-        <p class="intro-text"> Inleidende text voor de searchpage. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Suspendisse ornare, mauris ut convallis euismod, magna orci sodales neque.</p>
+    <h2 class="intro-kop">Titeltext voor de searchpage</h2>
+    <p class="intro-text"> Inleidende text voor de searchpage. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Suspendisse ornare, mauris ut convallis euismod, magna orci sodales neque.</p>
     <form action="search.php" class="intro-text" style="padding-top: 0px; margin-top: 0px !important;" method="post">
         <input type="text" name="search">
         <input type="submit" name="submit">
     </form>
     <div class="row">
-    <?php   
-    if (isset($shows)) {
-        foreach ($shows as $result) {
-            ?>
+        <?php
+        if (isset($shows)) {
+            if (!empty($shows)) {
+                foreach ($shows as $result) {
+                    ?>
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-12" style="padding-top: 24px">
+                        <div class="av-card">
+                            <img class="av-img"
+                                 src="http://placehold.jp/7464ac/ffffff/1920x1080.png?text=<?php echo $result['project_imagename']; ?>">
+                            <div class="av-text">
+                                <h2 class="av-card-kop"><?php echo $result['project_name']; ?></h2>
+                                <p class="av-card-text"><?php echo $result['project_description']; ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                }
+            } else {
+                echo 'Geen resultaten gevonden';
+            }
+        } else {
+            foreach ($results as $result) {
+                ?>
                 <div class="col-lg-4 col-md-6 col-sm-6 col-12" style="padding-top: 24px">
                     <div class="av-card">
                         <img class="av-img"
@@ -62,24 +81,9 @@ include('../includes/navbar.php');
                         </div>
                     </div>
                 </div>
-            <?php
-        }
-    } else {
-        foreach ($results as $result) {
-            ?>
-            <div class="col-lg-4 col-md-6 col-sm-6 col-12" style="padding-top: 24px">
-                <div class="av-card">
-                    <img class="av-img"
-                         src="http://placehold.jp/7464ac/ffffff/1920x1080.png?text=<?php echo $result['project_imagename']; ?>">
-                    <div class="av-text">
-                        <h2 class="av-card-kop"><?php echo $result['project_name']; ?></h2>
-                        <p class="av-card-text"><?php echo $result['project_description']; ?></p>
-                    </div>
-                </div>
-            </div>
-            <?php
-        }
-    } ?>
+                <?php
+            }
+        } ?>
     </div>
 </div>
 </body>
